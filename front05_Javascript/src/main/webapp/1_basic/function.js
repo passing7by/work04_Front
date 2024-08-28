a52fa0e176e09767ddc350bc962b377ac9a8fab7
passing7by <jinam0717@gmail.com>
2024-08-28 17:51:57 +0900

    feat : js 복습

---
+++ front05_Javascript/src/main/webapp/1_basic/function.js
@@ -0,0 +1,9 @@
+function changeText() {
+  document.querySelector("#hello").innerHTML = "Hello";
+  document.querySelector("#hello").style.color = "red";
+
+  document.querySelector("#edu").innerHTML = "BootCamp";
+  document.querySelector("#edu").style.color = "blue";
+} //changeText
+
+changeText();
